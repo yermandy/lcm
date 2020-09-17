93c54ef2ff1986d47c85a3d80fab08b5c4974c02
Andy Yermakov <yermaand@gmail.com>
2020-09-17 11:21:52 +0200

    fold and layers

--- fold.py
+++ fold.py
@@ -1,6 +1,10 @@
 from dataset.dataset import ListDataset
 from torch.utils.data import DataLoader
 from copy import deepcopy
+from dataset.dataset import combine_datasets, create_fold_stages
+from encoder_decoder import EncoderDecoder
+from resnet import resnet50
+from resnet_lcm import resnet50_lcm
 
 
 class Fold():
@@ -21,3 +25,33 @@ class Fold():
         self.lowest_val_mae = float('inf')
         self.best_epoch = -1
         self.number = number
+
+    def set_optimizer(self, optim, **kwargs):
+        self.optimizer = optim(self.net.parameters(), **kwargs)
+
+
+def create_folds(datasets, args, device):
+    enc_dec = EncoderDecoder()
+
+    combined_db, combined_folds, datasets_ages = combine_datasets(datasets, args.dataset_n)
+
+    loader_args = {
+        'num_workers': args.workers,
+        'batch_size': args.batch_size,
+        'shuffle': True
+    }
+
+    ## each fold consists of: [[trn], [val], [tst]] dictionaries
+    folds = []
+
+    for i, selected_folders in enumerate(combined_folds):
+        stages = create_fold_stages(combined_db, selected_folders)
+
+        if args.model == 'lcm':
+            net = resnet50_lcm(len(datasets), num_classes=len(enc_dec)).to(device)
+        else:
+            net = resnet50(num_classes=len(enc_dec)).to(device)
+
+        folds.append(Fold(net, stages, loader_args, i + 1))
+
+    return folds
--- layers.py
+++ layers.py
@@ -2,7 +2,7 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 
-from math import sqrt
+import numpy as np
 
 class MultiBiasedLinear(nn.Module):
     def __init__(self, in_features, out_features, n_datasets):
@@ -20,10 +20,10 @@ class MultiBiasedLinear(nn.Module):
         self.reset_parameters()
 
     def reset_parameters(self):
-        nn.init.kaiming_uniform_(self.weight, a=sqrt(5))
+        nn.init.kaiming_uniform_(self.weight, a=np.sqrt(5))
         for bias in self.biases:
             fan_in, _ = nn.init._calculate_fan_in_and_fan_out(self.weight)
-            bound = 1 / sqrt(fan_in)
+            bound = 1 / np.sqrt(fan_in)
             nn.init.uniform_(bias, -bound, bound)
 
     def forward(self, input, datasets_numbers):
@@ -34,60 +34,68 @@ class MultiBiasedLinear(nn.Module):
 
 
 class LCM(nn.Module):
-    def __init__(self, n_datasets, datasets_ages, ages=[], poly_degree=3):
+    def __init__(self, n_datasets, datasets_ages=None, ages=np.arange(1, 91)):
         super(LCM, self).__init__()
 
-        self.poly = nn.Parameter(torch.Tensor([ages ** d for d in range(poly_degree)]), requires_grad=False)
+        dataset_ages = np.arange(1, 91)
+        self.poly_deg_1 = nn.Parameter(torch.Tensor([dataset_ages ** d for d in range(2)]), requires_grad=False)
+        self.poly_deg_2 = nn.Parameter(torch.Tensor([dataset_ages ** d for d in range(3)]), requires_grad=False)
 
-        self.datasets_ages = nn.ParameterList([])
-        for d_ages in datasets_ages:
-            self.datasets_ages.append(nn.Parameter(torch.Tensor([d_ages]).T, requires_grad=False))
+        # self.datasets_ages = nn.ParameterList([])
+        # for d_ages in datasets_ages:
+        #     self.datasets_ages.append(nn.Parameter(torch.Tensor(d_ages), requires_grad=False))
 
         self.a_hat = nn.Parameter(torch.Tensor(ages), requires_grad=False)
         self.g_hat = nn.Parameter(torch.Tensor([-1, 1]), requires_grad=False)
 
-        self.zs = nn.ParameterList([])
         self.us = nn.ParameterList([])
         self.vs = nn.ParameterList([])
-        for i in range(n_datasets):
-            for dataset_poly_coeffs in [self.zs, self.us, self.vs]:
-                # TODO: initialize wisely
-                poly_coeffs = nn.Parameter(torch.Tensor(2, poly_degree))
-                nn.init.kaiming_uniform_(poly_coeffs, a=sqrt(5))
-                dataset_poly_coeffs.append(poly_coeffs)
+        self.zs = nn.ParameterList([])
 
-    def forward(self, input, datasets_numbers):
+        for i in range(n_datasets):
+            self.us.append(nn.Parameter(torch.Tensor([[0, 1, 0], [0, 1, 0]])))            
+            self.vs.append(nn.Parameter(torch.Tensor([[1, 0], [1, 0]])))
+            # self.zs.append(nn.Parameter(torch.Tensor([[5, 0], [-5, 0]])))
+            self.zs.append(nn.Parameter(torch.Tensor([[-1, -0.1, 0.0012], [1, 0.1, -0.0012]])))
+
+
+    def forward(self, input, datasets_numbers, return_PagIx=False):
 
         ## p(a,g|x)
         PagIx = F.softmax(input, dim=1)
 
         PaIag_PgIag_d = {}
-        
+
         for d in datasets_numbers:
             d = d.item()
             if d not in PaIag_PgIag_d:
-                zeta = (self.zs[d] @ self.poly).flatten()
-                mu = (self.us[d] @ self.poly).flatten()
-                sigma = (self.vs[d] @ self.poly).flatten()
-                norm = torch.exp(-0.5 * (mu - self.datasets_ages[d]) ** 2 / (sigma ** 2 + 1e-4)).sum(dim=0) + 1e-4
-                
-                ## p(ĝ|a,g)
-                PgIag = (self.g_hat * zeta.unsqueeze(1)).sigmoid()
+                mu = (self.us[d] @ self.poly_deg_2).view(-1, 1)
+                sigma = (self.vs[d] @ self.poly_deg_1).view(-1, 1)
+                gamma = (self.zs[d] @ self.poly_deg_2).view(-1, 1)
 
+                ## p(ĝ|a,g)
+                PgIag = (self.g_hat * gamma).sigmoid()
+                
                 ## p(â|a,g)
-                PaIag = torch.exp(-0.5 * (mu.unsqueeze(1) - self.a_hat) ** 2 / (sigma.unsqueeze(1) ** 2 + 1e-4)) / norm.unsqueeze(1)
+                PaIag = F.softmax((-0.5 * ((mu - self.a_hat) ** 2)) / (sigma ** 2 + 1e-8), dim=1)
 
                 PaIag_PgIag_d[d] = torch.cat((PaIag * PgIag[:, 0].unsqueeze(1), PaIag * PgIag[:, 1].unsqueeze(1)), dim=1)
 
-        outputs = torch.empty_like(input)
+        # outputs = torch.empty_like(input)
+        outputs = torch.empty((len(input), 180), device=input.device)
 
-        for i, p, d in zip(range(len(input)), PagIx, datasets_numbers):
+        for i, (p, d) in enumerate(zip(PagIx, datasets_numbers)):
             d = d.item()
 
             ## p(â|a,g) * p(ĝ|a,g)
             PaIag_PgIag = PaIag_PgIag_d[d]
 
             ## p(â,ĝ|x,d) = p(â|a,g) * p(ĝ|a,g) * p(a,g|x)            
-            outputs[i] = (PaIag_PgIag * p.unsqueeze(1)).sum(1)
+            # output = (PaIag_PgIag * p.unsqueeze(1)).sum(0)
+            # output = output / output.sum()
+            outputs[i] = (PaIag_PgIag * p.unsqueeze(1)).sum(0)
+
+        if return_PagIx:
+            return outputs, PagIx
 
         return outputs
